7addbdebd43c99a08d1aa078a1c81bb4992e002c
Stino <linksonder@gmail.com>
2023-02-06 16:11:46 +0100

    localhost werkt niet meer met mongodbb
    MongooseServerSelectionError: connect ECONNREFUSED 127.0.0.1:27017`

--- dataAccess/database.js
+++ dataAccess/database.js
@@ -4,7 +4,7 @@ mongoose.Promise = require('q').Promise;
 module.exports = function(){
 	if(mongoose.connection.readyState == 0){
 		// Get these from config
-		var uri = 'mongodb://localhost:27017/coursesDemo';
+		var uri = 'mongodb://127.0.0.1:27017/coursesDemo';
 		var options = {
 			db: { native_parser: true },
 			server: { poolSize: 5 },
